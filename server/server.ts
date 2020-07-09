1cfa0deb39937f61368569fb9c0e094a4309fdc2
wintyo <wintyo1024@gmail.com>
2020-07-09 14:23:49 +0900

    ログイン認証のサンプルコードを書く

---
+++ server/server.ts
@@ -0,0 +1,78 @@
+import path from 'path';
+import Express, { urlencoded } from 'express';
+import passport from 'passport';
+import { Strategy as LocalStrategy } from 'passport-local';
+import session from 'express-session';
+
+const app = Express();
+
+const PORT = 3000;
+const server = app.listen(PORT, () => {
+  console.log('server start!', PORT);
+});
+
+app.use(Express.json());
+app.use(urlencoded({ extended: false }));
+app.use(Express.static(path.resolve(__dirname, './public')));
+
+app.use(session({
+  resave: false,
+  saveUninitialized: false,
+  secret: 'passport test',
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(
+  {
+    usernameField: 'username',
+    passwordField: 'password',
+    passReqToCallback: true,
+    session: true,
+  },
+  (req, username, password, done) => {
+    if (username === "test" && password === "test") {
+      return done(null, username)
+    } else {
+      console.log("login error")
+      return done(null, false, { message: 'パスワードが正しくありません。' })
+    }
+  }
+));
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
+
+// ログインPOST遷移
+app.post('/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/login.html',
+  session: true,
+}));
+
+// ログイン後のユーザ名を取得する
+app.get('/api/user', (req, res) => {
+  if (!req.isAuthenticated()) {
+    res.send(null);
+    return;
+  }
+
+  res.send(req.user);
+});
+
+// ログインAPI
+app.post(
+  '/api/login',
+  passport.authenticate('local', {
+    session: true,
+  }),
+  (req, res) => {
+    res.send('success');
+  }
+);
