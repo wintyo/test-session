96da19b214722ee2ad040a9add15c78d319ed720
wintyo <wintyo1024@gmail.com>
2020-07-09 15:03:34 +0900

    proxyを使って認証情報を取得できることを確認

---
+++ webpack.config.ts
@@ -0,0 +1,35 @@
+import path from 'path';
+import * as webpack from 'webpack';
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const config: webpack.Configuration = {
+  entry: {
+    index: path.resolve(__dirname, './src/entry.js'),
+  },
+  mode: 'development',
+  devServer: {
+    contentBase: '.temp',
+    host: '0.0.0.0',
+    disableHostCheck: true,
+    hot: true,
+    inline: true,
+    overlay: true,
+    port: 3030,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+      }
+    },
+    historyApiFallback: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: path.resolve(__dirname, './src/index.html'),
+      hash: true,
+      inject: true,
+    })
+  ]
+};
+
+export default config;
